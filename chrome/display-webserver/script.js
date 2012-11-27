c7200537a497b2d0687df250416ac797f0ca4326
Ruslan Kabalin <r.kabalin@lancaster.ac.uk>
2012-11-27 16:03:03 +0000

    [chrome] display-webserver: display frontend
    by fetching fe falue from the cookie

--- chrome/display-webserver/script.js
+++ chrome/display-webserver/script.js
@@ -1,8 +1,14 @@
-chrome.extension.sendMessage({url: window.location.href, name: "MLE"}, function(response) {
-    // prepare div
-    var webserverDiv = document.createElement('div');
-    webserverDiv.innerHTML = "moodle-" + response.value;
-    webserverDiv.setAttribute('style', "position: fixed; bottom: 0; right: 0; background: lightgray; padding: 2px; opacity: 0.8;");
+// prepare div
+var webserverDiv = document.createElement('div');
+webserverDiv.setAttribute('style', "position: fixed; bottom: 0; right: 0; background: lightgray; padding: 2px 3px; opacity: 0.8;");
+document.body.appendChild(webserverDiv);
 
-    document.body.appendChild(webserverDiv);
+// retrieve frontend
+chrome.extension.sendMessage({url: window.location.href, name: "fe"}, function(response){
+    webserverDiv.innerHTML = response.value + ' > ';
+});
+
+// retrieve webserver
+chrome.extension.sendMessage({url: window.location.href, name: "MLE"}, function(response){
+    webserverDiv.innerHTML += response.value;
 });
